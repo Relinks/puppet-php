160b846d680fa7c9946959afe37d3dcc515fa073
Jeroen Jans <jeroen.jans@rorix.nl>
2017-08-10 08:17:48 +0200

    Install the given PHP repo package on RHEL

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -7,8 +7,10 @@
 #
 
 class php::repo::redhat (
-  $yum_repo = 'remi_php56',
+  $yum_repo = 'ius-release',
 ) {
-  contain "::yum::repo::${yum_repo}"
-  contain '::yum::repo::remi'
+  package { $yum_repo:
+    ensure  => 'latest',
+    require => Yumrepo['rorix'],
+  }
 }
